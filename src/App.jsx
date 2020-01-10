072bd3fb1e141384b413e21e72d07667bc3096e6
mustran <ademimusli000@gmail.com>
2020-01-10 09:54:14 +0100

    add sass and homepage styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Homepage from "./Homepage";
 
 function App() {
-  return <div>Hello World</div>;
+  return (<Homepage />)
 }
 
 export default App;
