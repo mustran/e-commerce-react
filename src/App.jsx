c5f3978e277bcc95a11c9256420c92525464b764
mustran <ademimusli000@gmail.com>
2020-01-29 16:14:49 +0100

    Clear App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 
-<<<<<<< HEAD
 
 function App() {
   return (
@@ -14,21 +13,6 @@ function App() {
 
     </div>
   )
-=======
-const HatsPage = () => {
-    return <div>HATS PAGE</div>;
-};
-
-function App() {
-    return (
-        <div>
-            <Switch>
-                <Route exact path="/" component={Homepage} />
-                <Route exact path="/hats" component={HatsPage} />
-            </Switch>
-        </div>
-    );
->>>>>>> 2307e7d569629592a3e3ebef474940c3dd554305
 }
 
 export default App;
